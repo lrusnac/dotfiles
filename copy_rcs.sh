8b96a7abf27f97a570d4ff1fda97e70cf0cc8f9e
Leonid <leonidrusnac4@gmail.com>
2017-10-17 10:31:38 +0200

    update

--- copy_rcs.sh
+++ copy_rcs.sh
@@ -4,12 +4,52 @@ if [ $# -eq 0 ]; then
 else
     CONFIG=$1
 fi
+
+if [ $CONFIG == 'all' -o $CONFIG == 'mac' ]; then
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+    echo "install brew packages"
+    cat homebrew-packages | xargs brew install
+    echo "install brew-cask packages"
+    cat homebrew-cask-packages | xargs brew cask install
+fi
+
 if [ $CONFIG == 'all' -o $CONFIG == 'vim' ]; then
-    sh ~/dotfiles/bin/installvimconfig.sh
+    sh bin/installvimconfig.sh
     cp ~/.vimrc{,_bk}
-    cp .vimrc ~/.vimrc
+    cp vimrc ~/.vimrc
     echo "Copied vimrc to ~/.vimrc. Old vimrc copied to ~/.vimrc_bk"
     vim +PluginInstall +qall
     echo "Installed vim plugins"
 fi
 
+if [ $CONFIG == 'all' -o $CONFIG == 'slate' ]; then
+    cp ~/.slate{,_bk}
+    cp slate ~/.slate
+    echo "Copied slate to ~/.slate. Old slate copied to ~/.slate_bk"
+fi
+
+if [ $CONFIG == 'all' -o $CONFIG == 'karabiner' ]; then
+    mkdir ~/.config/karabiner
+    cp ~/.config/karabiner/karabiner.json{,_bk}
+    cp karabiner.json ~/.config/karabiner/karabiner.json
+    echo "Copied karabiner.json to  ~/.config/karabiner/karabiner.json. Old slate copied to  ~/.config/karabiner/karabiner.json_bk"
+fi
+
+if [ $CONFIG == 'all' -o $CONFIG == 'zsh' ]; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+    cp ~/.zshrc{,_bk}
+    cp zshrc ~/.zshrc
+    echo "Copied zshrc to ~/.zshrc. Old zshrc copied to ~/.zshrc_bk"
+fi
+
+if [ $CONFIG == 'all' -o $CONFIG == 'git' ]; then
+    cp ~/.gitconfig{,_bk}
+    cp gitconfig ~/.gitconfig
+    echo "Copied gitconfig to ~/.gitconfig. Old gitconfig copied to ~/.gitconfig_bk"
+fi
+
+if [ $CONFIG == 'all' -o $CONFIG == 'bin' ]; then
+    cp bin/prune /usr/local/bin/prune
+fi
